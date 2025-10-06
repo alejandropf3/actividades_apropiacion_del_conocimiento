65916c7ddc77fccf05df4b4ff6bb3ada00fb186d
alejandropf3 <alejandropefa31@gmail.com>
2025-10-06 18:00:00 -0500

    Creamos el archivo JS punto 4 y realizamos el ejercicio 4

---
+++ punto4.js
@@ -0,0 +1,11 @@
+alert((20+10)/5+3*2-4*(20 + 10)/5+3*2 -4*(20+10)/5+3*2-4);
+//primero se hacen las operaciones dentro de los parentesis
+alert((30)/5+3*2-4*(30)/5+3*2 -4*(30)/5+3*2-4);
+//luego las divisiones y multiplicaciones
+alert(6+6-24+6 -24+6-24);
+//luego las sumas y restas de izquierda a derecha
+alert(-24);
+//el resultado final es -24
+
+
+

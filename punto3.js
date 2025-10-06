02c324d46bc5ee50db0777207b5a73c113a4247a
alejandropf3 <alejandropefa31@gmail.com>
2025-10-06 17:46:02 -0500

    Creamos el archivo JS punto 3 y desarrollamos el ejercicio 3

---
+++ punto3.js
@@ -0,0 +1,11 @@
+let visitas = 100; //Declaracion de variables
+
+visitas += 25; //Incrementamos el numero de visitas en 25
+
+visitas -= 10; //Disminuimos el numero de visitas en 10
+
+visitas *= 2; //Duplicamos el numero de visitas
+
+visitas = parseInt(visitas); //Comvertimos el dato
+
+alert("El numero de vistas total es: " + visitas); //Impresion de resultados
